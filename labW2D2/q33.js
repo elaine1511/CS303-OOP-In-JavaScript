483f9add643a8f1fafa559a061d4d5462ec1afe3
elaine1511 <elainetmo1511@gmail.com>
2021-03-30 09:22:24 -0500

    Create q33.js

---
+++ labW2D2/q33.js
@@ -0,0 +1,51 @@
+/*
+write a function that take a string and return an array where the nth indexed element gives 
+a count of the number of words of length n.
+["Every day is a great day for golf"] => output: [0,1,1,2,1,2]
+Input: string
+Output: array
+Process: 
+- split string into an array of each word
+- find the length of the result array by getting the longest word in the array of string
+- remove the duplicate word in the array of string
+- assign 0 indexed element to 0
+- loop through array of string inside the loop of the result array
+*/
+
+
+function indexedCountWords(string) {
+    let result = [];
+    result[0] = 0;
+    let arrLen = longestLength(string) + 1;
+    let arr = string.split(" ");
+    let arrFilter = removeDuplicates(arr);
+
+    for (let j = 1; j < arrLen; j++) {
+        let count = 0;
+        for (let i = 0; i < arrFilter.length; i++) {
+            if (arrFilter[i].length === j) {
+                count++;
+            }
+        }
+        result.push(count);
+    }
+    return result;
+}
+
+
+function removeDuplicates(array) {
+    return array.filter((a, b) => array.indexOf(a) === b)
+};
+
+function longestLength(str) {
+    let arr = str.split(" ");
+    let longest = arr[0].length;
+    for (let words of arr) {
+        if (words.length > longest) {
+            longest = words.length;
+        }
+    }
+    return longest;
+}
+
+console.log(indexedCountWords("Every day is a great day for golf"));
